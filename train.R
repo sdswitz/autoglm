fcd80780eb0d6ea4be5c1bacda68fa6e6773627e
Sam Switz <sds9bt@gmail.com>
2026-03-25 18:51:55 -0500

    add ShortTerm (<=36m) indicator with GFC and SBA_Portion interactions

--- train.R
+++ train.R
@@ -33,6 +33,7 @@ sba <- sba %>% mutate(
   Term3 = Term^3,
   Term4 = Term^4,
   HighSBA = as.integer(SBA_Portion > 0.75),
+  ShortTerm = as.integer(Term <= 36),
   DisbMonth = factor(format(as.Date(DisbursementDate, format="%d-%b-%y"), "%m")),
   TermBucket = cut(Term, breaks = c(0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 180, 240, 360, Inf),
                    labels = c("0-1yr", "1-2yr", "2-3yr", "3-4yr", "4-5yr", "5-6yr", "6-7yr", "7-8yr", "8-9yr", "9-10yr", "10-15yr", "15-20yr", "20-30yr", "30yr+"),
@@ -65,7 +66,8 @@ mod1 <- glm(PaidInFull ~ NewExist_f + LowDoc + IsRevLine + UrbanRural_f + NoEmp 
               Term2:GFC + Term3:GFC +
               Recession:TermBucket +
               Pre1989:TermBucket +
-              SameBankState:GFC,
+              SameBankState:GFC +
+              ShortTerm + ShortTerm:GFC + ShortTerm:SBA_Portion,
             data = sba_train, family = "binomial")
 
 # Predictions and evaluation (do not modify evaluate function — it lives in prepare.R)
